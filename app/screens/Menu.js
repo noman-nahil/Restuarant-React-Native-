800d40b94013ede1c09a69bb0566760306aae79b
noman <abdullahnoman1997@gmail.com>
2021-09-05 01:17:11 +0600

    Setting up project(screens,component,AppNavigator)

---
+++ app/screens/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+
+const Menu = () => {
+    return (
+        <View>
+            <Text>Menu Screen</Text>
+        </View>
+    )
+}
+
+export default Menu;
