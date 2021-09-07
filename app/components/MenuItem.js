71c83ccf864b87cccd08d83e0dd1b3e6ee0c4893
noman <abdullahnoman1997@gmail.com>
2021-09-07 16:48:30 +0600

    Styling MenuItem

--- app/components/MenuItem.js
+++ app/components/MenuItem.js
@@ -4,12 +4,37 @@ import { View, StyleSheet, TouchableHighlight, Image, Text } from 'react-native'
 const MenuItem = props => {
     return (
         <TouchableHighlight>
-            <View>
-                <Text>{props.item.menuname}</Text>
-                <Text>{props.item.description}</Text>
+            <View style={styles.container}>
+                <Image source={{ uri: props.item.images[0] }} style={styles.image} />
+                <View style={styles.details}>
+                    <Text style={styles.name}>{props.item.menuname}</Text>
+                    <Text style={styles.description} numberOfLines={2}>{props.item.description}</Text>
+                </View>
             </View>
         </TouchableHighlight>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        flexDirection: "row",
+        padding: 15
+    },
+    details: {
+        flex: 1,
+        marginLeft: 10,
+        justifyContent: "center"
+    },
+    image: {
+        width: 80,
+        height: 80
+    },
+    name: {
+        fontWeight: "500"
+    },
+    description: {
+        color: "#6e6969"
+    }
+})
 
 export default MenuItem;
